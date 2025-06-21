9d594bbaf72e7030e806ea610ff841be1d5769e1
EARL T <adhoc_vor@proton.me>
2025-06-21 13:37:38 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   libGCN.h
    	new file:   libgcn.c
    	new file:   main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "libGCN.h"
+int main(){
+CRD atl =INIT_CRD_DEG(33.64,-84.427);
+CRD det = INIT_CRD_DEG(42.409,-83.01);
+void SHOW_CRD_RAD( CRD atl );
+void SHOW_CRD_RAD( CRD det );
+return 1;
+}
